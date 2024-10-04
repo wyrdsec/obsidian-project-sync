df14203db680dbeff049d293fdad0ce15719aa39
wyrd <wyrdsec@protonmail.com>
2024-10-03 22:02:06 -0400

    Basic UI

--- main.ts
+++ main.ts
@@ -1,134 +1,57 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { ButtonComponent, Notice, Plugin, TextComponent } from "obsidian";
+import { existsSync } from "fs";
 
-// Remember to rename these classes and interfaces!
+class ProjsyncBlock {
+	path: string;
+	exclude: Array<string>;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	constructor(source: string) {
+		const rows = source.split("\n").filter((row) => row.length > 0);
+		for (let i=0; i < rows.length; i++) {
+			const words = rows[i].split(" ");
+			if (words.length > 0) {
+				switch (words[0]) {
+					case "path":
+						let path = words.slice(1).join('');
+						if (path.length == 0) { throw TypeError("Path is empty.") }
+						if (!existsSync(path)) { throw TypeError("Path does not exist.")}
+						this.path = path;
+						break;
+					
+					case "exclude":
+						let excludePath = words.slice(1).join('');
+						if (excludePath.length == 0) { break }
+						if (!this.exclude.includes(excludePath)) { this.exclude.push(excludePath) }
+						break;
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					default:
+						break;
 				}
 			}
-		});
+		}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		if (this.path == undefined) { throw SyntaxError("Path value was not set.") };
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+export default class ObsidianProjectSync extends Plugin {
+	async onload() {
+		this.registerMarkdownCodeBlockProcessor("projsync", (source, el, ctx) => {
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+			try {
+				const psb = new ProjsyncBlock(source);
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				var button = new ButtonComponent(el);
+				button.setIcon("sync");
+				button.setTooltip("Sync to filesystem", {delay: 0, placement: 'bottom'});
+				button.onClick((evt: MouseEvent) => {
+					new Notice("This is a notice!");
+				})
+			} catch(e: unknown) {
+				const displayError = el.createEl("div", { cls: "project-sync-displayerror" });
+				displayError.createEl("div", {text: `${(e as Error).name}`, cls: "project-sync-displayerror-name"});
+				displayError.createEl("small", {text: `${(e as Error).message}`, cls: "project-sync-displayerror-message"});
+			}
+		  });
 	}
 }
